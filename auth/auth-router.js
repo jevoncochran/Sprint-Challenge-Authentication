09b1fd4a7a1f0e62b20e659bf2a609b5f983dff3
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-28 13:33:05 -0300

    auth router and users model set up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,60 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const users = require('./users-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  users.add(user)
+    .then(added => {
+      res.status(201).json(added);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'unable to register user' });
+    })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  users.findBy({ username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          messsage: `Welcome ${user.username}`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'unable to login' });
+  })
 });
+
+function generateToken(user) {
+  const payload = {
+    user: user.username
+  };
+
+  const secret = 'top secret';
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
